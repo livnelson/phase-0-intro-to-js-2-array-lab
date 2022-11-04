641292e5900c926182876c1f719d7d24aa91ceef
Liv Nelson <livnelson@me.com>
2022-11-04 14:03:22 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    let newestCats = [name, ...cats];
+    return newestCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0, 2);
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
